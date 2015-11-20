bd17f927faa4af36e4d795a64adabd99616bce90
Andrea Zito <andrea.zito@u-hopper.com>
2015-11-20 17:20:58 +0100

    Added historyserver to list of images to build

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in hadoop namenode datanode resourcemanager nodemanager spark; do
+for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark; do
     ( cd $i && ./build.sh)
 done
