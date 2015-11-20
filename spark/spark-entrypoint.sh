9c7c73bddd98a5ee56a137f1ad7e4b27ea0b908b
Andrea Zito <andrea.zito@u-hopper.com>
2015-11-20 17:19:04 +0100

    Spark container now launches HistoryServer by default

--- spark/spark-entrypoint.sh
+++ spark/spark-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-export SPARK_DIST_CLASSPATH=$(hadoop classpath)
+for c in `printenv | perl -sne 'print "$1 " if m/^SPARK_CONF_(.+?)=.*/'`; do 
+    name=`echo ${c} | perl -pe 's/___/-/g; s/__/_/g; s/_/./g'`
+    var="SPARK_CONF_${c}"
+    value=${!var}
+    echo "Setting SPARK property $name=$value"
+    echo $name $value >> $SPARK_HOME/conf/spark-defaults.conf
+done 
 
 exec /entrypoint.sh $@
