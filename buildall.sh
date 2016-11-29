25397e37accd8cade249efaeb76ec88bddadb138
Andrea Zito <andrea.zito@u-hopper.com>
2016-11-29 16:46:23 +0100

    Updated Spark to 2.0.2

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark spark-notebook; do
+for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark; do
+    echo Building $i
     ( cd $i && ./build.sh)
 done
